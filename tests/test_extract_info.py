252d56414897887fb64c694de4e77868f9dd614b
Sylvie <wisepoison@gmail.com>
2019-06-30 13:20:54 -0400

    in process of rearranging

--- tests/test_extract_info.py
+++ tests/test_extract_info.py
@@ -9,17 +9,11 @@ import extract_info
 import extract_names
 import utils
 
-def correct_pattern_gen(items: List[str], suffix: str) -> str:
-    if len(items) == 1:
-        return items[0]+ "\n" + suffix
-    return f"{items[0]}\n{suffix}({correct_pattern_gen(items[1:], suffix)})?".replace("\n\n", "\n")
-    # awful hack please debug
 
 # R_good = "R1( R2( R3)?)??"
 # R_fail = "R1R2R3"
 # C_good = f"C1 {R_good} (C2 {R_good})"
 # G_good = f"G1({C_good})?G2
-# todo: test executation order
 # there's a few versions that are acceptable
 # g1, c1, r1, r2, c2, r1, r2, g2, c1, r1, r2, etc would make sense
 # but if g1 is wrong we need to immediately skip to g2
@@ -41,18 +35,14 @@ def correct_pattern_gen(items: List[str], suffix: str) -> str:
 # if this is an FSM there should be a regex for it
 # using a shorthand where [gcr][12] is one char
 
+def correct_pattern_gen(items: List[str], suffix: str) -> str:
+    if len(items) == 1:
+        return items[0]+ "\n" + suffix
+    return f"{items[0]}\n{suffix}({correct_pattern_gen(items[1:], suffix)})?".replace("\n\n", "\n")
+    # awful hack please debug
 
-Entry = Dict[str, List[str]]
 
-CASES: List[Entry]
-CASES = json.load(open("data/correct_cases.json", encoding="utf-8"))
-
-
-@pytest.fixture(params=CASES)
-def correct_case(request: Any) -> Entry:
-    return request.param
-
-def _trace_extract_info() -> Tuple[utils.Logger, Callable]:
+def trace_extract_info_nonfixture() -> Tuple[utils.Logger, Callable]:
     logger = utils.Logger()
     def wrap_logging(funcs: List[Callable]) -> List[Callable]:
         return [logger.logged(func) for func in funcs]
@@ -76,8 +66,20 @@ def _trace_extract_info() -> Tuple[utils.Logger, Callable]:
         return result
     return (logger, traced_extract_info)
 
-trace_extract_info = pytest.fixture(_trace_extract_info)
+trace_extract_info_fixture = pytest.fixture(
+    trace_extract_info_nonfixture, name="trace_extract_info"
+)
 
+
+Entry = Dict[str, List[str]]
+
+CASES: List[Entry]
+CASES = json.load(open("data/correct_cases.json", encoding="utf-8"))
+
+
+@pytest.fixture(params=CASES, name="correct_case")
+def correct_case_fixture(request: Any) -> Entry:
+    return request.param
 
 
 def test_cases(correct_case: Entry,
@@ -98,8 +100,8 @@ DIFFICULT_CASES = json.load(
         "data/incorrect_cases.json",
         encoding="utf-8"))
 
-@pytest.fixture(params=DIFFICULT_CASES)
-def difficult_case(request: Any) -> Entry:
+@pytest.fixture(params=DIFFICULT_CASES, name="difficult_case")
+def difficult_case_fixture(request: Any) -> Entry:
     return request.param
 
 def fd_print(text: str, end: str = "\n") -> None:
@@ -142,47 +144,9 @@ def test_difficult_cases(difficult_case: Entry) -> None:
                 indent=4
             )
             fd_print("marked as a correct example")
+            # just move this into the other one
     assert actual_case["names"] != difficult_case["names"]
 
-
-def classify_examples(entries: List[Entry],
-                      n: int, show_contact_info: bool,
-                      known_correct: List[Entry],
-                      known_incorrect: List[Entry]
-                      ) -> Iterable[Tuple[bool, Entry]]:
-    random.shuffle(entries)
-    classified = 0
-    while classified < n and entries:
-        entry = entries.pop()
-        if entry not in known_correct and entry not in known_incorrect:
-            correctness = ask(entry, show_contact_info)
-            classified += 1
-            yield (correctness, entry)
-
-def save_examples(entries: List[Entry], n: int,
-                  show_contact_info: bool = False,
-                  known_correct: List[Entry] = CASES,
-                  known_incorrect: List[Entry] = DIFFICULT_CASES) -> None:
-    examples: List[Tuple[bool, Entry]] = list(classify_examples(
-        entries, n, show_contact_info,
-        known_correct, known_incorrect
-    ))
-    correct: List[Entry]
-    incorrect: List[Entry]
-    correct, incorrect = (
-        [example for (type_, example) in examples if type_ == selected_type]
-        for selected_type in (True, False)
-    )
-    assert len(correct) + len(incorrect) == len(examples)
-    json.dump(
-        known_correct + correct,
-        open("data/correct_cases.json", "w", encoding="utf-8"),
-        indent=4)
-    json.dump(
-        known_incorrect + incorrect,
-        open("data/incorrect_cases.json", "w", encoding="utf-8"),
-        indent=4)
-    print(f"saved {len(correct)} correct, {len(incorrect)} incorrect examples")
 
 def test_last() -> None:
     # fake test
--- tests/tools.py
+++ tests/tools.py
@@ -23,3 +23,42 @@ def show_all_extractions(text: str) -> Dict[str, List[List[str]]]:
 
 
 
+def classify_examples(entries: List[Entry],
+                      n: int, show_contact_info: bool,
+                      known_correct: List[Entry],
+                      known_incorrect: List[Entry]
+                      ) -> Iterable[Tuple[bool, Entry]]:
+    random.shuffle(entries)
+    classified = 0
+    while classified < n and entries:
+        entry = entries.pop()
+        if entry not in known_correct and entry not in known_incorrect:
+            correctness = ask(entry, show_contact_info)
+            classified += 1
+            yield (correctness, entry)
+
+def save_examples(entries: List[Entry], n: int,
+                  show_contact_info: bool = False,
+                  known_correct: List[Entry] = CASES,
+                  known_incorrect: List[Entry] = DIFFICULT_CASES) -> None:
+    examples: List[Tuple[bool, Entry]] = list(classify_examples(
+        entries, n, show_contact_info,
+        known_correct, known_incorrect
+    ))
+    correct: List[Entry]
+    incorrect: List[Entry]
+    correct, incorrect = (
+        [example for (type_, example) in examples if type_ == selected_type]
+        for selected_type in (True, False)
+    )
+    assert len(correct) + len(incorrect) == len(examples)
+    json.dump(
+        known_correct + correct,
+        open("data/correct_cases.json", "w", encoding="utf-8"),
+        indent=4)
+    json.dump(
+        known_incorrect + incorrect,
+        open("data/incorrect_cases.json", "w", encoding="utf-8"),
+        indent=4)
+    print(f"saved {len(correct)} correct, {len(incorrect)} incorrect examples")
+
